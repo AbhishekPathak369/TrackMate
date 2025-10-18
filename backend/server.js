0731d1da1298c8d327040cb1c92aece309b6f29d
AbhishekPathak369 <pathakabhi290@gmail.com>
2025-10-18 20:25:52 +0530

    s

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const PORT = 3000;
+// const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // POST /getAttendance
 app.post('/getAttendance', async (req, res) => {
